95d9221371beeebfd593e1af5006f97f6ffc79ca
Kamotek <kamotek123@gmail.com>
2025-03-26 17:51:29 +0100

    fix: change name of vertex

--- lista1/src/graph/vertex.cpp
+++ lista1/src/graph/vertex.cpp
@@ -1,5 +1,28 @@
-//
-// Created by kamil on 23.03.2025.
-//
-
 #include "vertex.h"
+#include <utility>
+
+Vertex::Vertex(std::string name) : stopName(std::move(name)) {}
+
+const std::string& Vertex::getName() const {
+    return stopName;
+}
+
+const std::vector<Vertex*>& Vertex::getNeighbors() const {
+    return neighbors;
+}
+
+const std::vector<edge*>& Vertex::getEdges() const {
+    return edges;
+}
+
+void Vertex::addNeighbor(Vertex* neighbor) {
+    if (std::find(neighbors.begin(), neighbors.end(), neighbor) == neighbors.end()) {
+        neighbors.push_back(neighbor);
+    }
+}
+
+void Vertex::addEdge(edge* edgePtr) {
+    if (std::find(edges.begin(), edges.end(), edgePtr) == edges.end()) {
+        edges.push_back(edgePtr);
+    }
+}
